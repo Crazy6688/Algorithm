761141ae7632840f3c7f545e00defe7cfea5d381
dengyufeng <dyf7970268@163.com>
2020-07-24 22:55:04 +0800

    1234

--- LeetCode/Case_464.cs
+++ LeetCode/Case_464.cs
@@ -23,64 +23,55 @@ namespace LeetCode
 
         public void Test()
         {
-            var win1 = CanIWin(4, 6);
-            var win2 = CanIWin(10, 11);
-            var win3 = CanIWin(10, 40);
-            var win4 = CanIWin(18, 79);
+            //10,11,false
+            //4,6,true
+            //10,40,false
+            //18.79,true
+            //6,16,true
+            //var win1 = CanIWin(4, 6);
+            //var win2 = CanIWin(10, 11);
+            //var win3 = CanIWin(10, 40);
+            //var win4 = CanIWin(18, 79);
+            var win5= CanIWin(6, 16);
             //System.Diagnostics.Debug.WriteLine($"464:{win2}");
         }
 
 
         public bool CanIWin(int maxChoosableInteger, int desiredTotal)
         {
-            if (maxChoosableInteger * (maxChoosableInteger - 1) / 2 < desiredTotal)
+            if (maxChoosableInteger * (maxChoosableInteger + 1) / 2 < desiredTotal)
                 return false;
             if (maxChoosableInteger >= desiredTotal)
                 return true;
+            var map1 = new Boolean[1 << maxChoosableInteger];
+            var hhh1 = win1(maxChoosableInteger, desiredTotal, map1, 0);
 
-            var hhh = helper(maxChoosableInteger, desiredTotal, new Boolean[1 << maxChoosableInteger], 0);
+            var map2 = new Dictionary<int, bool>(1 << maxChoosableInteger);
+            var hhh2 = win2(maxChoosableInteger, desiredTotal, map2, 0);
 
 
-            //Console.WriteLine($"CanIWin=>{maxChoosableInteger},{desiredTotal}!");
-            //for (var i = 1; i <= maxChoosableInteger; i++)
-            //{
-            //    var www = win(1 << i, new Dictionary<int, int>() { { 1, i } }, maxChoosableInteger, 0, desiredTotal);
-            //    if (!www)
-            //        return false;
-            //}
-            if (hhh)
+            var hhh3 = win3(0, maxChoosableInteger, desiredTotal);
+
+
+            if (hhh1 && hhh2)
                 Console.WriteLine($"{maxChoosableInteger},{desiredTotal},先手稳赢!");
+            if (hhh1 != hhh3)
+                Console.WriteLine($"{maxChoosableInteger},{desiredTotal},ERROR!");
             return true;
         }
 
 
-        bool win(int flag, Dictionary<int, int> seq, int max, int total, int desireTotal)
-        {
-            //A表示先手 
-            var loop = BitOperations.PopCount((uint)flag) + 1;
-            //  var role = loop % 2 == 0 ? 'A' : 'B';
-
-            for (var i = 1; i <= max; i++)
-            {
-                if ((flag & (1 << i)) > 0)
-                    continue;
-                var newflag = flag;
-                newflag |= (1 << i);
-                seq[loop] = i;
-                var newTotal = total + i;
-
-                if (newTotal >= desireTotal ||
-                    win(newflag, seq, max, newTotal, desireTotal) == false)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-
-        }
-
-        public bool helper(int maxChoosableInteger, int desiredTotal, bool[] dp, int state)
+        /// <summary>
+        /// 该方法用于判定在已经各自取值的state状态后,是否在经过一次取数据就赢
+        /// 返回true,表示可以,返回false表示不可以
+        /// 因为每一个state值对应了一个当前各自取值的累加,所以只需要判定是否能再取一次值就胜利,如果能,表示稳赢
+        /// </summary>
+        /// <param name="maxChoosableInteger"></param>
+        /// <param name="desiredTotal"></param>
+        /// <param name="dp"></param>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        public bool win1(int maxChoosableInteger, int desiredTotal, bool[] dp, int state)
         {
             if (dp[state] != false)
                 return dp[state];
@@ -89,11 +80,9 @@ namespace LeetCode
                 int cur = 1 << (i - 1);
                 if ((cur & state) != 0)
                     continue;
-
-                // 如果当前选择使得累积和大于等于desiredTotal
-                // 又或者当前选择之后，另一个人的选择必输，说明当前必赢
+ 
                 if (desiredTotal - i <= 0 ||
-                    helper(maxChoosableInteger, desiredTotal - i, dp, state | cur) == false)
+                    win1(maxChoosableInteger, desiredTotal - i, dp, state | cur) == false)
                 {
                     return dp[state] = true;
                 }
@@ -101,6 +90,61 @@ namespace LeetCode
 
             // 无论怎么选都无法让对手输，那么就是自己输了
             return dp[state] = false;
+        }
+
+        /// <summary>
+        /// 和win1方法一样,用字典存放状态,效率比数组低
+        /// </summary>
+        /// <param name="maxChoosableInteger"></param>
+        /// <param name="desiredTotal"></param>
+        /// <param name="dp"></param>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        public bool win2(int maxChoosableInteger, int desiredTotal, Dictionary<int, bool> dp, int state)
+        {
+            if (dp.TryGetValue(state, out var has))
+                return has;
+
+            for (int i = 1; i <= maxChoosableInteger; i++)
+            {
+                int cur = 1 << (i - 1);
+                if ((cur & state) != 0)
+                    continue;
+
+                if (desiredTotal <= i ||
+                    win2(maxChoosableInteger, desiredTotal - i, dp, state | cur) == false)
+                {
+                    return dp[state] = true;
+                }
+            }
+
+            return dp[state] = false;
+        }
+
+        /// <summary>
+        /// 不采用记忆缓存,每次都累加,效率最低,但减少了空间
+        /// </summary>
+        /// <param name="flag"></param>
+        /// <param name="max"></param>
+        /// <param name="desireTotal"></param>
+        /// <returns></returns>
+        bool win3(int flag, int max, int desireTotal)
+        {
+            for (var i = 1; i <= max; i++)
+            {
+                if ((flag & (1 << i)) > 0)
+                    continue;
+                var newflag = flag;
+                newflag |= (1 << i);
+
+                if (desireTotal <= i || win3(newflag, max, desireTotal - i) == false)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
         }
 
     }
