48a838dd13eee1290d1de929d001570a09fc1d6b
Crazy6688 <dyf7970268@163.com>
2020-08-11 16:02:06 +0800

    1111

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -11,8 +11,31 @@ namespace LeetCode
             Console.WriteLine("Hello World!");
             var test = new Case_464();
             test.Test();
+            var p = new Program();
+
+            p.climbStairs(20);
+
         }
 
+
+
+
+        int[] cache = new int[100];
+
+        public int climbStairs(int n)
+        {
+            if (n <= 0)
+                return 0;
+            if (cache == null)
+                cache = new int[n + 1];
+            if (n <= 2)
+                return cache[n] = n;
+
+            if (cache[n] == 0)            
+                cache[n] = climbStairs(n - 1) + climbStairs(n - 2); 
+
+            return cache[n];
+        }
     }
 
     public class TreeNode
